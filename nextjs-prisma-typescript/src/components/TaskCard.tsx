cfb963f24dea34e22cc8e57fff3ad2898646a85d
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-21 18:32:56 -0500

    Redirecting to the edit page of the task on click

--- nextjs-prisma-typescript/src/components/TaskCard.tsx
+++ nextjs-prisma-typescript/src/components/TaskCard.tsx
@@ -12,4 +12,8 @@ function TaskCard({ task }: Props) {
     
     return (
         <div className='bg-gray-900 p-3 hover:bg-gray-800 hover:cursor-pointer'
+            onClick={() => {
+                router.push(`/tasks/edit/${task.id}`)
+            }}
+        >
         
