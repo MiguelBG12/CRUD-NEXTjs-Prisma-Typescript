e86e7104fe58706ae378b5769cb2515324bbd693
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-21 16:59:00 -0500

    Handles DELETE requests

--- nextjs-prisma-typescript/src/app/api/tasks/[id]/route.ts
+++ nextjs-prisma-typescript/src/app/api/tasks/[id]/route.ts
@@ -27,3 +27,5 @@ export async function PUT(request: Request, { params }: Params) {
     return NextResponse.json(taskUpdated)
 }
 
+export async function DELETE(request: Request, { params }: Params) {
+    
