b11ed4e9d0731ac3d98cdc9c134af3626f6e8919
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-21 18:28:08 -0500

    Logo or title of the application

--- nextjs-prisma-typescript/src/components/Navbar.tsx
+++ nextjs-prisma-typescript/src/components/Navbar.tsx
@@ -4,3 +4,8 @@ function Navbar() {
     return (
         <nav className="flex justify-between items-center py-4">
             <Link href="/">
+                <h3 className="text-2x1 font-bold">
+                    NextCRUD
+                </h3>
+            </Link>
+            
